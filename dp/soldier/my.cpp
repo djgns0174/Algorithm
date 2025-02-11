c5bb3b2b91bef709eda03040ac55c6df808ad94f
djgns0174 <djgns0174@naver.com>
2025-02-11 17:07:49 +0900

    [동적 프로그래밍] 병사 배치하기 LIS 알고리즘 공부 완료

--- dp/soldier/my.cpp
+++ dp/soldier/my.cpp
@@ -4,7 +4,29 @@ using namespace std;
 
 int main(void){
 
-    // 가장 긴 부분수열 LIS 알고리즘 적용 필요
-    
+    int n;
+    cin >> n;
+    // n = 7;
+
+    vector<int>inputArr(n);
+    vector<int>dp(n, 1);
+
+    for(int i=0; i<n; i++){
+        cin >> inputArr[i];
+    }
+
+    for(int i=1; i<n; i++){
+        for(int j=0; j<i; j++){
+            if(inputArr[i] < inputArr[j]){
+                if(dp[i] < dp[j]+1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+    }
+
+    int result = n - *std::max_element(dp.begin(), dp.end());
+    cout << result << endl;
+
     return 0;
 }
