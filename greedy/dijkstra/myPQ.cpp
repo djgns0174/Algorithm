2ddd5698a01e11f5a4dd5c55faed473c61be492c
djgns0174 <djgns0174@naver.com>
2025-02-13 20:08:15 +0900

    [그리디] 최단경로 다익스트라 알고리즘 우선순위 큐

--- greedy/dijkstra/myPQ.cpp
+++ greedy/dijkstra/myPQ.cpp
@@ -10,23 +10,23 @@ priority_queue<pair<int, int> > pq;
 
 int init(){
     graph[1].push_back({2,2});
-    graph[1].push_back({1,4});
-    graph[1].push_back({5,3});
+    graph[1].push_back({4,1});
+    graph[1].push_back({3,5});
 
     graph[2].push_back({3,3});
-    graph[2].push_back({2,4});
+    graph[2].push_back({4,2});
 
-    graph[3].push_back({3,2});
-    graph[3].push_back({5,6});
+    graph[3].push_back({2,3});
+    graph[3].push_back({6,5});
 
     graph[4].push_back({3,3});
-    graph[4].push_back({1,5});
+    graph[4].push_back({5,1});
 
-    graph[5].push_back({1,3});
-    graph[5].push_back({2,6});
+    graph[5].push_back({3,1});
+    graph[5].push_back({6,2});
 
     d[1] = 0;
-    pq.push({0,1});
+    pq.push({1,0});
 
     return 0;
 }
@@ -36,15 +36,16 @@ int main(void){
     init();
 
     while(!pq.empty()){
-        int dist = -pq.top().first;
-        int now = pq.top().second;
+        int now = pq.top().first;
+        int dist = -pq.top().second;
+        pq.pop();
         if(d[now] < dist) continue;
         for(int i=0; i<graph[now].size(); i++){
             int to = graph[now][i].first;
             int temp = graph[now][i].second + d[now];
             if(temp < d[to]) {
                 d[to] = temp;
-                pq.push({-d[to], to});
+                pq.push({to, -d[to]});
             }
         }
     }
