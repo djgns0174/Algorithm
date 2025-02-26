b6b00590dca79fb6bb86b82b606aa5a592b3bc6a
yehoon <djgns01631221@gmail.com>
2025-02-26 14:39:43 +0900

    [백준] 11047 동전 0

---
+++ baekjoon/11047.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main(void){
+
+    int arr[] = {1,2,3,4,5};
+    // int n, k;
+    // cin >> n >> k;
+    // cout << n << endl;
+
+    for(int i=0; i<5; i++){
+        cout << arr[i];
+    }
+    cout << endl;
+
+    return 0;
+}
