c60566fd27d5830bd578970338687017e98aa9ed
djgns0174 <djgns0174@naver.com>
2025-03-27 02:45:04 +0900

    [백준] 2156 포도주 시식

--- baekjoon/2156.cpp
+++ baekjoon/2156.cpp
@@ -21,6 +21,9 @@ int main(void){
     dp[1] = arr[1];
     dp[2] = arr[1] + arr[2];
     dp[3] = arr[1] + arr[3];
+    if(dp[3] < dp[2]){
+        dp[3] = dp[2];  // 틀린이유 -> dp[3]의 경우를 두가지만 생각했는데 3가지 경우를 생각해서 비교해야됨.
+    }
     int tmp = arr[2] + arr[3];
     if(dp[3] < tmp){
         sequenceCheck = true;
@@ -29,22 +32,40 @@ int main(void){
 
     if(n >= 4){
         for(int i=4; i<=n; i++){
+            // 연속 세잔 마실 가능성이 있는 경우
             if(sequenceCheck){
+                // 현재 잔을 마시는 경우
                 dp[i] = dp[i-2] + arr[i];
+                sequenceCheck = false;
+                int tmp = dp[i-3] + arr[i-1] + arr[i];
+                if(dp[i] < tmp) {
+                    dp[i] = tmp;
+                    sequenceCheck = true;
+                    /* 
+                        현재 잔을 마시는 경우
+                        예제 케이스에서 dp[4]일때 25와 28 비교 필요
+                    */
+                }
                 if(dp[i] < dp[i-1]){
-                    dp[i] = dp[i-1]; 
+                    dp[i] = dp[i-1];
                     /*
+                        현재 잔을 안 마시는 경우
                         예제 케이스에서 dp[6]일때 33과 26 비교필요
                     */
                 }
-                sequenceCheck = false;
             } else {
                 dp[i] = dp[i-1] + arr[i];
                 sequenceCheck = true;
+
+                int tmp = dp[i-2] + arr[i];
+                if(dp[i] < tmp) {
+                    dp[i] = tmp;
+                    sequenceCheck = false;
+                }
             }
         }
     }
-    
+
     cout << dp[n] << "\n";
 
     return 0;
